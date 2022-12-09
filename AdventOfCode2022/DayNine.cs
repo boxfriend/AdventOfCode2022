f7bf46887ae065d6e3429c2adeffe2090ae40dd0
Boxfriend <cawills394@gmail.com>
2022-12-09 16:55:15 -0500

    Day nine completed

---
+++ AdventOfCode2022/DayNine.cs
@@ -0,0 +1,88 @@
+﻿namespace AdventOfCode2022;
+internal class DayNine : AdventSolution
+{
+    public IEnumerable<string> GetSolutions (IEnumerable<string> input)
+    {
+        var visitedPositionCount = GetVisitedPositions(input, 2);
+        var visitedLength10 = GetVisitedPositions(input, 10);
+        
+        return new string[]
+        {
+            $"Number of positions tail visited: {visitedPositionCount}",
+            $"Number of positions with length 10: {visitedLength10}"
+        };
+    }
+
+    private static int GetVisitedPositions(IEnumerable<string> input, int ropeLength)
+    {
+        var visitedPositions = new HashSet<Point>
+        {
+            Point.Zero
+        };
+        var ropePositions = new Point[ropeLength];
+        Array.Fill(ropePositions, new(0, 0));
+        foreach (var command in input)
+        {
+            var targetPosition = ropePositions[0] + GetDirection(command);
+
+            while (ropePositions[0] != targetPosition)
+            {
+                ropePositions[0] = ropePositions[0].MoveTowards(targetPosition);
+                for (var i = 1; i < ropePositions.Length; i++)
+                {
+                    var prevPosition = ropePositions[i-1];
+                    if (ropePositions[i].IsAdjacent(prevPosition))
+                        break;
+
+                    ropePositions[i] = ropePositions[i].MoveTowards(prevPosition);
+                }
+                visitedPositions.Add(ropePositions[^1]);
+            }
+        }
+        return visitedPositions.Count;
+    }
+
+    private static Point GetDirection(string command)
+    {
+        var commandParts = command.Split(' ');
+        var distance = int.Parse(commandParts[1]);
+
+        return commandParts[0] switch
+        {
+            "R" => Point.Right,
+            "L" => Point.Left,
+            "U" => Point.Up,
+            "D" => Point.Down,
+            _ => Point.Zero
+        } * distance;
+    }
+
+    private record struct Point(int X, int Y)
+    {
+        public static Point Zero => new(0, 0);
+        public static Point Up => new(0, 1);
+        public static Point Down => new(0, -1);
+        public static Point Right => new(1, 0);
+        public static Point Left => new(-1, 0);
+
+        public static Point operator + (Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
+        public static Point operator - (Point a, Point b) => new(a.X - b.X, a.Y - b.Y);
+        public static Point operator * (Point a, int b) => new(a.X * b, a.Y * b);
+
+        public bool IsAdjacent(Point toCheck)
+        {
+            var distance = Direction(this, toCheck);
+
+            return Math.Abs(distance.X) <= 1 && Math.Abs(distance.Y) <= 1;
+        }
+
+        public static Point Direction (Point from, Point to) => to - from;
+
+        public Point MoveTowards(Point to)
+        {
+            var direction = Direction(this, to);
+
+            return this + new Point(Math.Sign(direction.X), Math.Sign(direction.Y));
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -2,50 +2,21 @@
 
 const string inputFolder = @"D:\Users\cawil\Documents\C# Stuff\AdventOfCode2022\Input";
 
-Console.WriteLine("Day One: Calorie Counting");
-string dayOneFile = Path.Join(inputFolder, "dayone.txt");
-var dayOneOutput = await SolutionGetter.GetSolutions<DayOne>(dayOneFile);
-foreach (var s in dayOneOutput)
-    Console.WriteLine(s);
+await GetSolution<DayOne>("dayone.txt", "Day One: Calorie Counting");
+await GetSolution<DayTwo>("daytwo.txt", "Day Two: Rock Paper Scissors");
+await GetSolution<DayThree>("daythree.txt", "Day Three: Rucksack Reorganization");
+await GetSolution<DayFour>("dayfour.txt", "Day Four: Camp Cleanup");
+await GetSolution<DayFive>("dayfive.txt", "Day Five: Supply Stacks");
+await GetSolution<DaySix>("daysix.txt", "Day Six: Tuning Trouble");
+await GetSolution<DaySeven>("dayseven.txt", "Day Seven: No Space Left On Device");
+await GetSolution<DayEight>("dayeight.txt", "Day Eight: Treetop Tree House");
+await GetSolution<DayNine>("daynine.txt", "Day Nine: Rope Bridge");
 
-Console.WriteLine("Day Two: Rock Paper Scissors");
-string dayTwoFile = Path.Join(inputFolder, "daytwo.txt");
-var dayTwoOutput = await SolutionGetter.GetSolutions<DayTwo>(dayTwoFile);
-foreach (var s in dayTwoOutput)
-    Console.WriteLine(s);
-
-Console.WriteLine("Day Three: Rucksack Reorganization");
-string dayThreeFile = Path.Join(inputFolder, "daythree.txt");
-var dayThreeOutput = await SolutionGetter.GetSolutions<DayThree>(dayThreeFile);
-foreach (var s in dayThreeOutput)
-    Console.WriteLine(s);
-
-Console.WriteLine("Day Four: Camp Cleanup");
-string dayFourFile = Path.Join(inputFolder, "dayfour.txt");
-var dayFourOutput = await SolutionGetter.GetSolutions<DayFour>(dayFourFile);
-foreach (var s in dayFourOutput)
-    Console.WriteLine(s);
-
-Console.WriteLine("Day Five: Supply Stacks");
-string dayFiveFile = Path.Join(inputFolder, "dayfive.txt");
-var dayFiveOutput = await SolutionGetter.GetSolutions<DayFive>(dayFiveFile);
-foreach (var s in dayFiveOutput)
-    Console.WriteLine(s);
-
-Console.WriteLine("Day Six: Tuning Trouble");
-string daySixFile = Path.Join(inputFolder, "daysix.txt");
-var daySixOutput = await SolutionGetter.GetSolutions<DaySix>(daySixFile);
-foreach (var s in daySixOutput)
-    Console.WriteLine(s);
-
-Console.WriteLine("Day Seven: No Space Left On Device");
-string daySevenFile = Path.Join(inputFolder, "dayseven.txt");
-var daySevenOutput = await SolutionGetter.GetSolutions<DaySeven>(daySevenFile);
-foreach (var s in daySevenOutput)
-    Console.WriteLine(s);
-
-Console.WriteLine("Day Eight: Treetop Tree House");
-string dayEightFile = Path.Join(inputFolder, "dayeight.txt");
-var dayEightOutput = await SolutionGetter.GetSolutions<DayEight>(dayEightFile);
-foreach (var s in dayEightOutput)
-    Console.WriteLine(s);
+async static Task GetSolution<T>(string fileName, string dayTitle) where T : AdventSolution, new()
+{
+    Console.WriteLine(dayTitle);
+    var file = Path.Join(inputFolder, fileName);
+    var output = await SolutionGetter.GetSolutions<T>(file);
+    foreach (var s in output)
+        Console.WriteLine(s);
+}
